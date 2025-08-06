0579183ed432d1f9c374bdc5c6f48856ef110057
redburningice <fabi1998@live.de>
2025-08-06 06:13:46 +0200

    cp1

--- src/main/java/ext/fabian/App.java
+++ src/main/java/ext/fabian/App.java
@@ -1,15 +1,21 @@
 package ext.fabian;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * JavaFX App
@@ -23,11 +29,22 @@ public class App extends Application {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+
+        ObservableList<String> countries = FXCollections.observableArrayList("germany", "italy", "japan");
+        ListView<String> listView = new ListView<>(countries);
+
 //        scene = new Scene(loadFXML("primary"), 640, 480);
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Scene scene = new Scene(new StackPane(l, listView), 640, 480);
         scene.getStylesheets().add(getClass().getResource("/ext/fabian/style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
+
+        Map<String, String> map = new HashMap<>();
+
+        String value = String.valueOf("123");
+
+        List<String> hello = List.of("Hello", "1");
+
     }
 
     static void setRoot(String fxml) throws IOException {
